ee4f8e599cde207082d1dc06a70e6d3847beb10f
Leonardo A <brinfo.leo@gmail.com>
2021-10-01 09:26:45 +0100

    finished exercise 002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,32 +1,37 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return person.city.toLowerCase().trim() === "manchester";
 }
+  function getBusNumbers(people) {
+    if (people === undefined) throw new Error("people is required");
+    let maxPeople = 40;
+    let nBus = Math.ceil(people / maxPeople);
+    return nBus;
+  }
 
-function getBusNumbers(people) {
-  if (people === undefined) throw new Error("people is required");
-  // Your code here!
-}
+  function countSheep(arr) {
+    if (arr === undefined) throw new Error("arr is required");
+    let sheep = 0;
+    for (let x in arr) {
+      if (arr[x].trim().toLowerCase() === "sheep") sheep++
+    }
+    return sheep;
+  }
 
-function countSheep(arr) {
-  if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
-}
+  function hasMPostCode(person) {
+    if (person === undefined) throw new Error("person is required");
+    return person.address.postCode.trim().charAt(0).toLowerCase() === "m" && person.address.city.trim().toLowerCase() === "manchester";
+  }
 
-function hasMPostCode(person) {
-  if (person === undefined) throw new Error("person is required");
-  // Your code here!
-}
-
-module.exports = {
-  getFillings,
-  isFromManchester,
-  countSheep,
-  getBusNumbers,
-  hasMPostCode
-};
+  module.exports = {
+    getFillings,
+    isFromManchester,
+    countSheep,
+    getBusNumbers,
+    hasMPostCode
+  };
