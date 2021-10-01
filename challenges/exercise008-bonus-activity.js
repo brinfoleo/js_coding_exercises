1852b3cabdab49accb65fa0819e625067b85d217
Leonardo A <brinfo.leo@gmail.com>
2021-10-01 13:00:21 +0100

    finished exercise 008

--- challenges/exercise008-bonus-activity.js
+++ challenges/exercise008-bonus-activity.js
@@ -1,32 +1,34 @@
 // Spread Syntax Bonus activity
 
-function printColours(colours){
+function printColours(colours) {
     if (!colours) throw new Error("colour is required");
-    // Your solution using the spread operator here!
+    return [...colours];
 }
 
-function shoppingList(list, newItem){
+function shoppingList(list, newItem) {
     if (!list) throw new Error("list is required");
     if (!newItem) throw new Error("newItem is required");
-    // Your solution using the spread operator here!
+    return [...list, ...newItem];
 }
 
-function highestNumber(numbers){
+function highestNumber(numbers) {
     if (!numbers) throw new Error("numbers are required");
-    // Your solution using the spread operator here!
+    return Math.max(...numbers);
 }
 
-function splitThatString(string){
+function splitThatString(string) {
     if (!string) throw new Error("string is required");
-    // Your solution using the spread operator here!
+    return [...string];
 }
 
 
 // Optional Chaining Bonus activity!
 
-function addressLookUp(user){
+function addressLookUp(user) {
     if (!user) throw new Error("user is required");
     // Your solution using optional chaining here!
+    return {...user.address}.postcode;
+
 }
 
 
